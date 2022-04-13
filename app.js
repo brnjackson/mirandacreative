ff7437e0dc672f47eac0755eaa59beea0ca2ac35
Brittney Jackson <brnjackson@yahoo.com>
2022-04-13 07:14:57 -0400

    changed zindex for opened cards

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ expand.forEach((element) => {
         document.querySelector("#text_hidden1").style.display = "block";
         document.querySelector("#collapse1").style.display = "block";      
         element.style.display = "none";
-        document.querySelector(".card1").style.zIndex = "150";
+        document.querySelector(".card1").style.zIndex = "100";
         break;
       case "2 expand":
         document.getElementById("text_hidden2").style.display = "block";
@@ -51,37 +51,37 @@ collapse.forEach((element) => {
         document.getElementById("text_hidden1").style.display = "none";
         element.style.display = "none";
         document.getElementById("1 expand").style.display = "block";
-        document.querySelector(".card1").style.zIndex = "250";
+        document.querySelector(".card1").style.zIndex = "initial";
         break;
       case "collapse2":
         document.getElementById("text_hidden2").style.display = "none";
         element.style.display = "none";
         document.getElementById("2 expand").style.display = "block";
-        document.querySelector(".card2").style.zIndex = "230";
+        document.querySelector(".card2").style.zIndex = "initial";
         break;
       case "collapse3":
         document.getElementById("text_hidden3").style.display = "none";
         element.style.display = "none";
         document.getElementById("3 expand").style.display = "block";
-        document.querySelector(".card3").style.zIndex = "210";
+        document.querySelector(".card3").style.zIndex = "initial";
         break;
       case "collapse4":
         document.getElementById("text_hidden4").style.display = "none";
         element.style.display = "none";
         document.getElementById("4 expand").style.display = "block";
-        document.querySelector(".card4").style.zIndex = "190";
+        document.querySelector(".card4").style.zIndex = "initial";
         break;
       case "collapse5":
         document.getElementById("text_hidden5").style.display = "none";
         element.style.display = "none";
         document.getElementById("5 expand").style.display = "block";
-        document.querySelector(".card5").style.zIndex = "170";
+        document.querySelector(".card5").style.zIndex = "initial";
         break;
       case "collapse6":
         document.getElementById("text_hidden6").style.display = "none";
         element.style.display = "none";
         document.getElementById("6 expand").style.display = "block";
-        document.querySelector(".card6").style.zIndex = "150";
+        document.querySelector(".card6").style.zIndex = "initial";
         break;
     }
   });
